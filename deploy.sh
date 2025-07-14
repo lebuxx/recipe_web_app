0ed824eff78792c50952736ca349dafda2ed7f84
LeBulli <153615219+LeBulli@users.noreply.github.com>
2025-07-14 22:52:45 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Stop and remove the existing frontend and backend containers, if running
+docker-compose -f deploy.yml down
+
+# Remove old images
+docker rmi web-app-backend web-app-frontend -f
+
+# Build and start new containers
+docker-compose -f deploy.yml up --build -d
+
+docker system prune -af
